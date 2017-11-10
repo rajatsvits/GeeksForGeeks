f8bf3d0177b3d83a5e27947865716e2a7b4be78a
rajatsvits <rajatmahajansvits@gmail.com>
2017-11-10 17:57:37 +0530

    Create countNumberOfWaysXCanBeObtainedWithSumOfNumberOfPowerN.cpp

---
+++ countNumberOfWaysXCanBeObtainedWithSumOfNumberOfPowerN.cpp
@@ -0,0 +1,38 @@
+// C++ program to count number of ways
+// to express x as sum of n-th power
+// of unique natural numbers.
+#include <bits/stdc++.h>
+using namespace std;
+
+// num is current num.
+int countWaysUtil(int x, int n, int num)
+{
+	// Base cases
+	//static int i=0;
+	int val = (x - pow(num, n));
+	if (val == 0)
+		return 1;
+	if (val < 0)
+		return 0;
+    //cout<<val<<" "<<i<<endl;
+    //i++;
+	// Consider two possibilities, num is
+	// included and num is not included.
+	return countWaysUtil(val, n, num + 1) +
+		countWaysUtil(x, n, num + 1);
+}
+
+// Returns number of ways to express
+// x as sum of n-th power of two.
+int countWays(int x, int n)
+{
+	return countWaysUtil(x, n, 1);
+}
+
+// Driver code
+int main()
+{
+	int x = 100, n = 1;
+	cout << countWays(x, n);
+	return 0;
+}
